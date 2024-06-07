80594117adb0d4d9ab7562f1b58de4c3d0515816
Andarol <oleksandrzaichko@gmail.com>
2024-06-07 18:11:46 +0300

    Added timesleep, that you dont need to run terraform apply 2 times

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,6 @@
 
 module "Autoscaling_instances" {
+  depends_on =[google_compute_network.static,time_sleep.wait_60_seconds]
   source     = "./modules/Autoscaling_instances"
   network    = google_compute_network.static.id
   subnetwork = google_compute_subnetwork.sub_for_instances.id
@@ -7,12 +8,14 @@ module "Autoscaling_instances" {
 }
 
 module "Compute_instance" {
+  depends_on =[google_compute_network.static,time_sleep.wait_60_seconds]
   source     = "./modules/Compute_instance"
   network    = google_compute_network.static.id
   subnetwork = google_compute_subnetwork.my_custom_subnet_for_grafane1.id
 }
 
 module "PSQL_DB" {
+  depends_on =[google_compute_network.static,time_sleep.wait_60_seconds]
   source  = "./modules/PSQL_DB"
   network = google_compute_network.static.id
   #vpc_peering_to_db = google_service_networking_connection.private_vpc_connection.id
@@ -20,16 +23,20 @@ module "PSQL_DB" {
 }
 
 module "dns" {
+  depends_on =[google_compute_network.static,time_sleep.wait_60_seconds]
   source         = "./modules/dns"
   global_address = google_compute_global_address.default.address
 
 }
 module "firewalls" {
+  depends_on =[google_compute_network.static,time_sleep.wait_60_seconds]
   source  = "./modules/firewalls"
   network = google_compute_network.static.id
+
 }
 
 module "load_balancer" {
+  depends_on =[google_compute_network.static,time_sleep.wait_60_seconds]
   source            = "./modules/load_balancer"
   instance_template = module.Autoscaling_instances.instance_template
   global_address    = google_compute_global_address.default.id
@@ -38,4 +45,5 @@ module "load_balancer" {
 
 module "NAT" {
   source = "./modules/NAT"
+  depends_on =[google_compute_network.static,time_sleep.wait_60_seconds]
 }
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -2,28 +2,44 @@ resource "google_compute_network" "static" {
   name                            = "my-network"
   delete_default_routes_on_create = true
   auto_create_subnetworks         = false
+  
+}
+resource "time_sleep" "wait_120_seconds" {
+  depends_on = [google_compute_network.static]
+
+  create_duration = "120s"
+}
+
+# This resource will create (at least) 30 seconds after null_resource.previous
+resource "null_resource" "next" {
+  depends_on = [time_sleep.wait_120_seconds]
 }
 
 resource "google_compute_global_address" "default" {
   name = "globaladdress"
+  depends_on = [time_sleep.wait_120_seconds]
 }
 
 resource "google_compute_subnetwork" "sub_for_instances" {
   name          = "my-custom-subnet"
   ip_cidr_range = "10.10.0.0/24"
   network       = google_compute_network.static.name
+  depends_on = [time_sleep.wait_120_seconds]
+  
 }
 
 resource "google_compute_subnetwork" "my_custom_subnet_for_grafane1" {
   name          = "my-custom-subnet-for-grafane1"
   ip_cidr_range = "10.10.10.0/24"
   network       = google_compute_network.static.name
+  depends_on = [time_sleep.wait_120_seconds]
 }
 
 resource "google_compute_subnetwork" "my_custom_subnet_for_postgres" {
   name          = "my-custom-subnet-for-postgres"
   ip_cidr_range = "10.10.20.0/24"
   network       = google_compute_network.static.name
+  depends_on = [time_sleep.wait_120_seconds]
 }
 
 
