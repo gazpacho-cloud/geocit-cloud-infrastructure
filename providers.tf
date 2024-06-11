d34077702205aa7c2adc6654f45e2672083d547a
Serpantiner <85799080+Serpantiner@users.noreply.github.com>
2024-06-11 11:13:00 +0300

    Delete providers.tf

--- providers.tf
+++
@@ -1,7 +0,0 @@
-provider "google" {
-  credentials = file("${path.module}/../ferrous-terrain-422816-i0-b0707ebf99c4.json")
-  project     = var.project
-  region      = var.region
-}
-
-data "google_client_config" "default" {}
